2f3a178ffcdd12078eeffac7fbbdf52d9210dd5a
Pawel Mazur <mpawe1993@gmail.com>
2025-05-24 15:05:05 +0200

    FE be changes

---
+++ pistolrange-server/src/main/java/pl/pistolrange/pistolrange_server/PistolrangeServerApplication.java
@@ -0,0 +1,13 @@
+package pl.pistolrange.pistolrange_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistolrangeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistolrangeServerApplication.class, args);
+	}
+
+}
---
+++ pistolrange-server/src/test/java/pl/pistolrange/pistolrange_server/PistolrangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pistolrange.pistolrange_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistolrangeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
